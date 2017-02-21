19da89ee74e0fa6104033e86d4c3c19d8f6598a1
Erik Demaine <edemaine@mit.edu>
2017-02-21 10:20:48 -0500

    Backup to Amazon Cloud Drive via acd_cli. Fix #93

--- .backup/backup.sh
+++ .backup/backup.sh
@@ -2,3 +2,5 @@
 cd "`dirname "$0"`"
 ssh ubuntu@coauthor mongodump --db coauthor
 rsync -a ubuntu@coauthor:dump/coauthor/ coauthor-backup/
+acd_cli mkdir /coauthor-backup/`date +%Y-%m-%d`
+acd_cli ul coauthor-backup/* /coauthor-backup/`date +%Y-%m-%d`
