5eacb9df9b80a399c83497431455e17b6e9cbb5f
Erik Demaine <edemaine@mit.edu>
2017-02-24 09:59:15 -0500

    add quiet

--- .backup/backup.sh
+++ .backup/backup.sh
@@ -4,4 +4,4 @@ ssh ubuntu@coauthor mongodump --db coauthor
 rsync -a ubuntu@coauthor:dump/coauthor/ coauthor-backup/
 acd_cli sync
 acd_cli mkdir /coauthor-backup/`date +%Y-%m-%d`
-acd_cli ul -o coauthor-backup/* /coauthor-backup/`date +%Y-%m-%d`
+acd_cli ul -q -o coauthor-backup/* /coauthor-backup/`date +%Y-%m-%d`
