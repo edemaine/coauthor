31774960995fc85bd2d8eb8436dfcb4f2b45090a
Erik Demaine <edemaine@mit.edu>
2017-02-24 09:54:06 -0500

    add sync, overwrite

--- .backup/backup.sh
+++ .backup/backup.sh
@@ -2,5 +2,6 @@
 cd "`dirname "$0"`"
 ssh ubuntu@coauthor mongodump --db coauthor
 rsync -a ubuntu@coauthor:dump/coauthor/ coauthor-backup/
+acd_cli sync
 acd_cli mkdir /coauthor-backup/`date +%Y-%m-%d`
-acd_cli ul coauthor-backup/* /coauthor-backup/`date +%Y-%m-%d`
+acd_cli ul -o coauthor-backup/* /coauthor-backup/`date +%Y-%m-%d`
