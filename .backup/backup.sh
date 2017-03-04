4701cff27d032608e157e9cc82b86daab187897d
Erik Demaine <edemaine@mit.edu>
2017-03-04 14:12:38 -0500

    20 retries

--- .backup/backup.sh
+++ .backup/backup.sh
@@ -4,4 +4,15 @@ ssh ubuntu@coauthor mongodump --db coauthor
 rsync -a ubuntu@coauthor:dump/coauthor/ coauthor-backup/
 acd_cli sync
 acd_cli mkdir /coauthor-backup/`date +%Y-%m-%d`
-acd_cli ul -q -o coauthor-backup/* /coauthor-backup/`date +%Y-%m-%d`
+count=0
+while ! acd_cli ul -q -o coauthor-backup/* /coauthor-backup/`date +%Y-%m-%d`
+do
+  echo Trying again... $count
+  acd_cli sync
+  acd_cli ul -q -o coauthor-backup/* /coauthor-backup/`date +%Y-%m-%d`
+  count=`expr $count + 1`
+  if [ $count > 20 ]
+  then
+    break
+  fi
+done
