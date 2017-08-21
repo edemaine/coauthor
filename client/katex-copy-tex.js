f750255202aea7d3ebf163b2589410712f4add26
Erik Demaine <edemaine@mit.edu>
2017-08-21 17:14:54 -0400

    Update copy-tex

--- client/katex-copy-tex.js
+++ client/katex-copy-tex.js
@@ -9,29 +9,35 @@ const copyDelimiters = {
 
 // Replace .katex elements with their TeX source (<annotation> element).
 // Modifies fragment in-place.  Useful for writing your own 'copy' handler.
-window.katexReplaceWithTex = function katexReplaceWithTex(fragment) {
+window.katexReplaceWithTex = function(fragment) {
     const katexs = fragment.querySelectorAll('.katex');
     // Replace .katex elements with their annotation (TeX source) descendant,
     // with inline delimiters.
-    katexs.forEach(function(element) {
+    for (let i = 0; i < katexs.length; i++) {
+        const element = katexs[i];
         const texSource = element.querySelector('annotation');
         if (texSource) {
-            element.replaceWith(texSource);
+            if (element.replaceWith) {
+                element.replaceWith(texSource);
+            } else {
+                element.parentNode.replaceChild(texSource, element);
+            }
             texSource.innerHTML = copyDelimiters.inline[0] +
                 texSource.innerHTML + copyDelimiters.inline[1];
         }
-    });
+    }
     // Switch display math to display delimiters.
-    fragment.querySelectorAll('.katex-display annotation').forEach(
-        function(element) {
-            element.innerHTML = copyDelimiters.display[0] +
-                element.innerHTML.substr(copyDelimiters.inline[0].length,
-                    element.innerHTML.length - copyDelimiters.inline[0].length
-                    - copyDelimiters.inline[1].length)
-                + copyDelimiters.display[1];
-        });
+    const displays = fragment.querySelectorAll('.katex-display annotation');
+    for (let i = 0; i < displays.length; i++) {
+        const element = displays[i];
+        element.innerHTML = copyDelimiters.display[0] +
+            element.innerHTML.substr(copyDelimiters.inline[0].length,
+                element.innerHTML.length - copyDelimiters.inline[0].length
+                - copyDelimiters.inline[1].length)
+            + copyDelimiters.display[1];
+    }
     return fragment;
-}
+};
 
 // Global copy handler to modify behavior on .katex elements.
 document.addEventListener('copy', function(event) {
@@ -43,11 +49,15 @@ document.addEventListener('copy', function(event) {
     if (!fragment.querySelector('.katex')) {
         return;  // default action OK if no .katex elements
     }
-    event.clipboardData.setData('text/plain',
-        katexReplaceWithTex(fragment).textContent);
     // Preserve usual HTML copy/paste behavior.
-    event.clipboardData.setData('text/html',
-        selection.getRangeAt(0).cloneContents());
+    const html = [];
+    for (let i = 0; i < fragment.childNodes.length; i++) {
+        html.push(fragment.childNodes[i].outerHTML);
+    }
+    event.clipboardData.setData('text/html', html.join(''));
+    // Rewrite plain-text version.
+    event.clipboardData.setData('text/plain',
+        window.katexReplaceWithTex(fragment).textContent);
     // Prevent normal copy handling.
     event.preventDefault();
 });
