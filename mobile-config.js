e16b020e0ae43fc72c7c2098ebf7b07e2e21b170
Erik Demaine <edemaine@mit.edu>
2017-10-05 16:44:36 -0400

    Add author, email

--- mobile-config.js
+++ mobile-config.js
@@ -1,5 +1,9 @@
 App.info({
   id: 'org.erikdemaine.coauthor',
   name: 'Coauthor',
-  version: '1.0.0'
+  version: '1.0.0',
+  author: 'Erik Demaine',
+  email: 'edemaine+coauthor@mit.edu'
 });
+
+App.accessRule('*');
