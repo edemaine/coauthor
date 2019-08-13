edemaine/coauthor

commit 8c296382cb0cd1d6790f8691e3ebbf25a33b54ab

Fix messageImageTransform calls
* Remove redundant call during formatBody (and fix #352), as it's always
  called during a template autorun (once it was modified to also get
  called when message is unfolded, after a `Meteor.defer` delay).
* This might also fix unreported "double rotation upon upload" issue.
* Remove resize event handlers when template destroyed.

client/message.coffee
@@ -561,10 +561,11 @@ Template.submessage.onRendered ->
 
   ## Image rotation
   @autorun =>
-    data = Template.currentData()
-    messageImageTransform.call @
+    data = Template.currentData()  ## update whenever message does
+    messageFolded.get data._id     ## update when message is unfolded
+    Meteor.defer => messageImageTransform.call @
   window.addEventListener 'resize',
-    _.debounce (=> messageImageTransform.call @), 100
+    @onResize = _.debounce (=> messageImageTransform.call @), 100
 
   ## Update side-by-side height settings when leaving editing mode
   ## and/or changing side-by-side preview setting.
@@ -577,6 +578,9 @@ Template.submessage.onRendered ->
         preview.height
       else
         'auto'
+
+Template.submessage.onDestroyed ->
+  window.removeEventListener 'resize', @onResize if @onResize?
 
 ## Cache EXIF orientations, as files should be static
 image2orientation = {}
@@ -913,9 +917,6 @@ Template.submessage.helpers
     #console.log 'rendering', @_id
     history = messageHistory.get(@_id) ? @
     body = history.body
-    ## Apply image settings (e.g. rotation) on embedded images and image files
-    t = Template.instance()
-    Meteor.defer -> messageImageTransform.call t
     return body unless body
     ## Don't show raw view if editing (editor is a raw view)
     if messageRaw.get(@_id) and not Template.instance().editing?.get()

client/messagePDF.coffee
@@ -8,6 +8,7 @@ Template.messagePDF.onCreated ->
   @fit = new ReactiveVar 'page'
 
 Template.messagePDF.onDestroyed ->
+  window.removeEventListener 'resize', @onResize if @onResize?
   `import('/imports/disappear')`.then (disappear) =>
     disappear.untrack @container
 
@@ -17,7 +18,7 @@ Template.messagePDF.onRendered ->
     @fit.get()
     tooltipUpdate()
   @container = @find 'div.pdf'
-  window.addEventListener 'resize', _.debounce (=> @resize?()), 100
+  window.addEventListener 'resize', @onResize = _.debounce (=> @resize?()), 100
   `import('pdfjs-dist')`.then (pdfjs) =>
     pdfjs.GlobalWorkerOptions.workerSrc = '/pdf.worker.min.js'  ## in /public
     @autorun =>

commit ef45d918c86f848eb1c0040054472a87f3857d86

Fix EXIF-based rotation in initial load
8c296382cb0cd1d6790f8691e3ebbf25a33b54ab introduced a bug where it
wouldn't trigger a rotation when the EXIF orientation initially loaded.

client/message.coffee
@@ -563,6 +563,7 @@ Template.submessage.onRendered ->
   @autorun =>
     data = Template.currentData()  ## update whenever message does
     messageFolded.get data._id     ## update when message is unfolded
+    messageRotate data             ## update when file orientation gets loaded
     Meteor.defer => messageImageTransform.call @
   window.addEventListener 'resize',
     @onResize = _.debounce (=> messageImageTransform.call @), 100