a95213ebfe19544baf97f51b2db011110e5bd70f
Erik Demaine <edemaine@mit.edu>
2020-02-22 15:40:43 -0500

    Switch Node image for new Meteor

--- .deploy/mup.js
+++ .deploy/mup.js
@@ -18,7 +18,7 @@ module.exports = {
       one: {}
     },
     docker: {
-      image: 'abernix/meteord:node-8.4.0-base', 
+      image: 'abernix/meteord:node-12-base',
       stopAppDuringPrepareBundle: false
     },
     buildOptions: {
