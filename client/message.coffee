edemaine/coauthor

commit 75d67d6aa0fd9367d7814647b57016b61b755217

Fix escaping use of authors, especially coauthors bootstrap (fix #512)

client/message.coffee
@@ -2470,7 +2470,7 @@ MessageActions = React.memo ({message, can, editing, tabindex0}) ->
   </Dropdown>
 
 messageAuthorSubtitle = (message, author) -> ->
-  if updated = message.authors[author]
+  if updated = message.authors[escapeUser author]
     (if message.creator == author
       "Created this message and last edited "
     else
@@ -2481,7 +2481,8 @@ messageAuthorSubtitle = (message, author) -> ->
 
 MessageAuthor = React.memo ({message, also}) ->
   if also
-    also = _.clone message.authors
+    also = {}
+    also[unescapeUser author] for author of message.authors
   count = 0
   <div className="author text-right">
     {'(by '}

server/bootstrap.coffee
@@ -107,7 +107,7 @@ Messages.find
 .forEach (message) ->
   usernames ?= allUsernames()  # load just once
   for diff in messageDiffsExpanded message
-    coauthors = (author for author of diff.authors)
+    coauthors = (unescapeUser author for author of diff.authors)
     coauthors.push ...(atMentions diff, usernames)
     coauthors = _.uniq coauthors
     unless _.isEqual coauthors, lastCoauthors

commit 490619a0ebc4e3e62ed099d6d74acec2b253d4c4

Fix display of shadow editors in history view
Bug introduced when unescaping in 75d67d6a

client/message.coffee
@@ -2741,7 +2741,7 @@ messageAuthorSubtitle = (message, author) -> ->
 export MessageAuthor = React.memo ({message, also}) ->
   if also
     also = {}
-    also[unescapeUser author] for author of message.authors
+    also[unescapeUser author] = updated for author, updated of message.authors
   count = 0
   <div className="author text-right">
     {'(by '}
@@ -2753,7 +2753,7 @@ export MessageAuthor = React.memo ({message, also}) ->
          subtitle={messageAuthorSubtitle message, author}/>
       </React.Fragment>
     }
-    {if also and not _.isEmpty also
+    {console.log (_.keys also); if also and not _.isEmpty also
       for author, updated of also
         <React.Fragment key={author}>
           {', ' if count++}